61b7e3a22ecf44541264adc0ea8c768b84225dc9
Omar Zunic <contact@omarzunic.com>
2022-04-14 03:44:07 +0200

    Pretty code.

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,220 +1,197 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Scripts.Enemy
 {
     public class EnemyAI : MonoBehaviour
-{
-    private Transform target;
-    private Transform myTransform;
- 
-    Vector3 storeTarget;
-    private Vector3 newTargetPos;
-    bool savePos;
-    bool overrideTarget;
-    Transform obstacle;
-    public List<Vector3> EscapeDirections = new List<Vector3>();
- 
-    //AI Checks
-    public bool chasing;
-    public bool evading;
- 
-    //AI stuff
-    public float rotationSpeed;
-    public float maxDist;
-    public float minDist;
-    public float fireDist;
- 
-    //AI Speeds
-    public float moveSpeed;
-
-    private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
-        myTransform = transform;
-    }
- 
-    private void FixedUpdate()
-    {
-        //Find Distance to target
-        var distance = (target.position - myTransform.position).magnitude;
- 
-        if(chasing)
-        {
-            //Rotate to look at player
-            var position1 = myTransform.position;
-            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - position1), rotationSpeed * Time.deltaTime);
-            //Move towards the player
-            position1 += myTransform.forward * (moveSpeed * Time.deltaTime);
-            myTransform.position = position1;
+        public List<Vector3> EscapeDirections = new List<Vector3>();
 
-            //Moves too far away from player
-            if (distance > maxDist)
-            {
-                chasing = false;
-            }
- 
-            //Attack if close enough
-            if (distance < fireDist)
-            {
-                //insert attack commands here when ready
-            }
- 
-            if (distance < minDist)
-            {
-                var position = myTransform.position;
-                myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(position - target.position), rotationSpeed * Time.deltaTime);
-                position += myTransform.forward * (moveSpeed * Time.deltaTime);
-                myTransform.position = position;
-            }
-        }
-        else
-        {
-            if (distance < maxDist)
-            {
-                chasing = true;
-            }
-        }
- 
-        ObstacleAvoidance(transform.forward, 0);
- 
-    }
+        //AI Checks
+        public bool chasing;
+        public bool evading;
 
-    private void ObstacleAvoidance(Vector3 direction, float offsetX)
-    {
-        var hit = Rays(direction, offsetX);
- 
-        for(var i = 0; i < hit.Length -1; i++)
+        //AI stuff
+        public float rotationSpeed;
+        public float maxDist;
+        public float minDist;
+        public float fireDist;
+
+        //AI Speeds
+        public float moveSpeed;
+        private Transform myTransform;
+        private Vector3 newTargetPos;
+        private Transform obstacle;
+        private bool overrideTarget;
+        private bool savePos;
+
+        private Vector3 storeTarget;
+        private Transform target;
+
+        private void Start()
         {
-            //So we dont detect ourself as a hit collision
-            if (hit[i].transform.root.gameObject == this.gameObject) continue;
-            if(!savePos)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            myTransform = transform;
+        }
+
+        private void FixedUpdate()
+        {
+            //Find Distance to target
+            var distance = (target.position - myTransform.position).magnitude;
+
+            if (chasing)
             {
-                storeTarget = target.position;
-                obstacle = hit[i].transform;
-                savePos = true;
+                //Rotate to look at player
+                var position1 = myTransform.position;
+                myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+                    Quaternion.LookRotation(target.position - position1), rotationSpeed * Time.deltaTime);
+                //Move towards the player
+                position1 += myTransform.forward * (moveSpeed * Time.deltaTime);
+                myTransform.position = position1;
+
+                //Moves too far away from player
+                if (distance > maxDist) chasing = false;
+
+                //Attack if close enough
+                if (distance < fireDist)
+                {
+                    //insert attack commands here when ready
+                }
+
+                if (distance < minDist)
+                {
+                    var position = myTransform.position;
+                    myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
+                        Quaternion.LookRotation(position - target.position), rotationSpeed * Time.deltaTime);
+                    position += myTransform.forward * (moveSpeed * Time.deltaTime);
+                    myTransform.position = position;
+                }
             }
- 
-            FindEscapeDirections(hit[i].collider);
-        }
- 
-        if (EscapeDirections.Count > 0)
-        {
-            if (!overrideTarget)
+            else
             {
-                newTargetPos = GetClosests();
-                overrideTarget = true;
+                if (distance < maxDist) chasing = true;
             }
-        }
-        var distance = Vector3.Distance(transform.position, target.position);
 
-        if (!(distance < 5)) return;
-        if (savePos)
-        {
-            //if we reach the target
-            target.position = storeTarget;
-            savePos = false;
+            ObstacleAvoidance(transform.forward, 0);
         }
-        else
-        {
-            //if we had waypoints
-            //index++
-        }
-        overrideTarget = false;
-        EscapeDirections.Clear();
-    }
 
-    private Vector3 GetClosests()
-    {
-        var clos = EscapeDirections[0];
-        var distance = Vector3.Distance(transform.position, EscapeDirections[0]);
- 
-        foreach(var dir in EscapeDirections)
+        private void ObstacleAvoidance(Vector3 direction, float offsetX)
         {
-            var tempDistance = Vector3.Distance(transform.position, dir);
-            if (!(tempDistance < distance)) continue;
-            distance = tempDistance;
-            clos = dir;
-        }
-        return clos;
- 
-    }
+            var hit = Rays(direction, offsetX);
 
-    private void FindEscapeDirections(Collider col)
-    {
-        //Check for obstacles above
-        var direct1 = col.transform.position + new Vector3(0, col.bounds.extents.y * 2 + 5, 0 );
-        if(Physics.Raycast(col.transform.position, col.transform.up, out _, col.bounds.extents.y * 2 + 5))
-        {
- 
-        }
-        else
-        {
-            //if there is something above
-
-            if (!EscapeDirections.Contains(direct1))
+            for (var i = 0; i < hit.Length - 1; i++)
             {
-                EscapeDirections.Add(direct1);
-            }
-        }
- 
-        //Check for obstacles below
-        if(Physics.Raycast(col.transform.position, -col.transform.up, out _, col.bounds.extents.y * 2 + 5))
-        {
- 
-        }
-        else
-        {
-            //if there is something below
-            var dir = col.transform.position + new Vector3(0, -col.bounds.extents.y * 2 - 5, 0 );
- 
-            if (!EscapeDirections.Contains(dir))
-            {
-                EscapeDirections.Add(dir);
-            }
-        }
-        //Check for obstacles to the Right
-        var direct2 = col.transform.position + new Vector3(col.bounds.extents.x * 2 + 5, 0, 0 );
-        if(Physics.Raycast(col.transform.position, col.transform.right, out _, col.bounds.extents.x * 2 + 5))
-        {
- 
-        }
-        else
-        {
-            //if there is something to the right
+                //So we don't detect ourself as a hit collision
+                if (hit[i].transform.root.gameObject == gameObject) continue;
+                if (!savePos)
+                {
+                    storeTarget = target.position;
+                    obstacle = hit[i].transform;
+                    savePos = true;
+                }
 
-            if (!EscapeDirections.Contains(direct2))
+                FindEscapeDirections(hit[i].collider);
+            }
+
+            if (EscapeDirections.Count > 0)
+                if (!overrideTarget)
+                {
+                    newTargetPos = GetClosests();
+                    overrideTarget = true;
+                }
+
+            var distance = Vector3.Distance(transform.position, target.position);
+
+            if (!(distance < 5)) return;
+            if (savePos)
             {
-                EscapeDirections.Add(direct2);
+                //if we reach the target
+                target.position = storeTarget;
+                savePos = false;
+            }
+
+            overrideTarget = false;
+            EscapeDirections.Clear();
+        }
+
+        private Vector3 GetClosests()
+        {
+            var clos = EscapeDirections[0];
+            var distance = Vector3.Distance(transform.position, EscapeDirections[0]);
+
+            foreach (var dir in EscapeDirections)
+            {
+                var tempDistance = Vector3.Distance(transform.position, dir);
+                if (!(tempDistance < distance)) continue;
+                distance = tempDistance;
+                clos = dir;
+            }
+
+            return clos;
+        }
+
+        private void FindEscapeDirections(Collider col)
+        {
+            //Check for obstacles above
+            var direct1 = col.transform.position + new Vector3(0, col.bounds.extents.y * 2 + 5, 0);
+            if (Physics.Raycast(col.transform.position, col.transform.up, out _, col.bounds.extents.y * 2 + 5))
+            {
+            }
+            else
+            {
+                //if there is something above
+
+                if (!EscapeDirections.Contains(direct1)) EscapeDirections.Add(direct1);
+            }
+
+            //Check for obstacles below
+            if (Physics.Raycast(col.transform.position, -col.transform.up, out _, col.bounds.extents.y * 2 + 5))
+            {
+            }
+            else
+            {
+                //if there is something below
+                var dir = col.transform.position + new Vector3(0, -col.bounds.extents.y * 2 - 5, 0);
+
+                if (!EscapeDirections.Contains(dir)) EscapeDirections.Add(dir);
+            }
+
+            //Check for obstacles to the Right
+            var direct2 = col.transform.position + new Vector3(col.bounds.extents.x * 2 + 5, 0, 0);
+            if (Physics.Raycast(col.transform.position, col.transform.right, out _, col.bounds.extents.x * 2 + 5))
+            {
+            }
+            else
+            {
+                //if there is something to the right
+
+                if (!EscapeDirections.Contains(direct2)) EscapeDirections.Add(direct2);
+            }
+
+            //Check for obstacles to the Left
+            if (Physics.Raycast(col.transform.position, -col.transform.right, out _, col.bounds.extents.x * 2 + 5))
+            {
+            }
+            else
+            {
+                //if there is something to the right
+                direct2 = col.transform.position + new Vector3(-col.bounds.extents.x * 2 - 5, 0, 0);
+
+                if (!EscapeDirections.Contains(direct2)) EscapeDirections.Add(direct2);
             }
         }
-        //Check for obstacles to the Left
-        if(Physics.Raycast(col.transform.position, -col.transform.right, out _, col.bounds.extents.x * 2 + 5))
-        {
- 
-        }
-        else
-        {
-            //if there is something to the right
-            direct2 = col.transform.position + new Vector3(-col.bounds.extents.x * 2 - 5, 0, 0 );
 
-            if (!EscapeDirections.Contains(direct2)) EscapeDirections.Add(direct2);
+        private RaycastHit[] Rays(Vector3 direction, float offsetX)
+        {
+            var position = transform.position;
+            var ray = new Ray(position + new Vector3(offsetX, 0, 0), direction);
+            Debug.DrawRay(position + new Vector3(offsetX, 0, 0), direction * (10 * moveSpeed), Color.red);
+
+            var distanceToLookAhead = moveSpeed * 5;
+            //Adjust 5 to proper radius around object to pick up raycast hits
+            var results = new RaycastHit[] { };
+            Physics.SphereCastNonAlloc(ray, 5, results, distanceToLookAhead);
+            return results;
         }
     }
-
-    private RaycastHit[] Rays(Vector3 direction, float offsetX)
-    {
-        var position = transform.position;
-        var ray = new Ray(position + new Vector3(offsetX,0,0), direction);
-        Debug.DrawRay(position + new Vector3(offsetX,0,0), direction * (10 * moveSpeed), Color.red);
- 
-        var distanceToLookAhead = moveSpeed * 5;
-        //Adjust 5 to proper radius around object to pick up raycast hits
-        var results = new RaycastHit[] { };
-        Physics.SphereCastNonAlloc(ray, 5, results, distanceToLookAhead);
-        return results;      
-    }
- 
- 
-}
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -9,62 +9,16 @@ public class EnemyController : MonoBehaviour
     public Transform enemy;
     public int health;
     public float speed;
-    public Transform player;
-    public bool isMoving;
-    private Vector3 newPosition = Vector3.zero;
-
-    // Update is called once per frame
-    void Update()
+    
+    public void TakeDamage(int damage)
     {
-
-    }
-
-    private void MoveRandomly()
-    {
-        // move towards a random point around the enemy
-        // wait until the enemy is at the new position
-        // then move again
-        if (isMoving)
+        health -= damage;
+        if (health <= 0)
         {
-            enemy.position = Vector3.MoveTowards(enemy.position, newPosition, speed * Time.deltaTime);
-            enemy.LookAt(newPosition);
-            if (enemy.position == newPosition)
-            {
-                isMoving = false;
-            }
+            Destroy(enemy.gameObject);
         }
-        else
-        {
-            float angle = Random.Range(0, 45);
-            float distance = Random.Range(5, 50);
-            newPosition = enemy.position + Quaternion.AngleAxis(angle, Vector3.up) * enemy.forward + Quaternion.AngleAxis(angle, Vector3.right) * enemy.forward * distance;
-
-            isMoving = true;
-        }
-        
-    }
-    
-
-    private void Move()
-    {
-        FlyTowardPlayer();
-        LookAtPlayer();
-    }
-    
-    private void FlyTowardPlayer(){
-        enemy.position = Vector3.MoveTowards(enemy.position, player.position, speed * Time.deltaTime);
     }
 
-    private void LookAtPlayer()
-    {
-        Vector3 dir = player.position - enemy.position;
-        dir.y = 0;
-        enemy.rotation = Quaternion.LookRotation(dir);
-        //enemy.LookAt(player);
-    }
-    
 
-    
-
-    }
+}
 
