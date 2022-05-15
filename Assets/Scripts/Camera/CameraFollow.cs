c03e597cfb8ea274dbe5f3e007ccb9383e26ca58
Omar Zunic <contact@omarzunic.com>
2022-05-15 16:37:56 +0200

    stuff.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,34 +1,31 @@
-using System;
 using UnityEngine;
 
 namespace Scripts.Camera
 {
     public class CameraFollow : MonoBehaviour
     {
-
         public Transform TPP;
         public Transform FPP;
         public Transform spaceship;
         public bool firstperson;
-        
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.F)) firstperson = !firstperson;
-            
-            if (spaceship!=null&&!firstperson)
+
+            if (spaceship != null && !firstperson)
             {
                 transform.position = TPP.position;
                 transform.LookAt(spaceship.transform);
                 transform.SetParent(null);
             }
-            if (firstperson&&spaceship!=null)
+
+            if (firstperson && spaceship != null)
             {
                 transform.position = FPP.position;
                 transform.SetParent(FPP);
                 transform.rotation = FPP.rotation;
             }
         }
-
-       
     }
 }
--- Assets/Scripts/World/ScoreKeeper.cs
+++ Assets/Scripts/World/ScoreKeeper.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class ScoreKeeper : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int score = 0;
-    public int enemiesKilled = 0;
-    public int currentWave = 0;
+    public int score;
+    public int enemiesKilled;
+    public int currentWave;
     public Text enemiesKilledText;
     public Text endScore;
     public Text WaveCounter;
-    
+
 
     private void Update()
     {
